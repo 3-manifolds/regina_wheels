bbc3acc9e45650825fe2e67c3e4378c37604b716
Nathan Dunfield <nathan@dunfield.info>
2024-07-28 19:45:06 -0500

    Temp move to other sources of libxml2 and tokyocabinet as unhyperbolic is migrating

--- extras/sageRegina/config.py
+++ extras/sageRegina/config.py
@@ -1,7 +1,7 @@
-tokyocabinet_uri  = 'http://sageRegina.unhyperbolic.org/sources/tokyocabinet-1.4.48.tar.gz'
+tokyocabinet_uri  = 'https://dbmx.net/tokyocabinet/tokyocabinet-1.4.48.tar.gz'
 tokyocabinet_dir  = 'tokyocabinet-1.4.48'
 
-libxml_uri        = 'http://sageRegina.unhyperbolic.org/sources/libxml2-2.9.3.tar.gz'
+libxml_uri        = 'http://xmlsoft.org/sources/libxml2-2.9.3.tar.gz'
 libxml_dir        = 'libxml2-2.9.3'
 
 regina_hash      = '3fc31fc43'  # 7.3 + iteration speed patch
