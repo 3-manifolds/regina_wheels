2b9339a7ae5741c1d7513fd513f7163a01955bf9
Nathan Dunfield <nathan@dunfield.info>
2025-08-28 21:54:27 -0500

    disable testsuite

--- setup.py
+++ setup.py
@@ -385,6 +385,7 @@ class package_patch_regina(SystemCommand):
         'cd regina_*/python/testsuite; mv docstrings.out.' + suffix + ' docstrings.out',
         'cd regina_*/python/testsuite; mv repr.out.' + suffix + ' repr.out',
         'cd regina_*/engine; rm -rf swift',
+        'cd regina_*/engine; rm -rf testsuite',
         ]
 
 class package_retrieve_tokyocabinet(CompoundCommand):
