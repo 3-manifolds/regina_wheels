d19bc1d0b45afa4bee6c6c1a45cc5e4da9843670
Nathan Dunfield <nathan@dunfield.info>
2025-10-27 19:02:22 -0500

    Moving to the 7.4.1 code

--- extras/sageRegina/config.py
+++ extras/sageRegina/config.py
@@ -4,6 +4,6 @@ tokyocabinet_dir  = 'tokyocabinet-1.4.48'
 libxml_uri        = 'http://xmlsoft.org/sources/libxml2-2.9.3.tar.gz'
 libxml_dir        = 'libxml2-2.9.3'
 
-regina_hash      = 'ee51922a0992855dcb3d7b82'  # 7.4
+regina_hash      = '3b1a2da19d70afa80476be'  # pre-release of 7.4.1 
 regina_uri       = 'https://github.com/regina-normal/regina.git'
 regina_dir       = 'regina_%s' % regina_hash
