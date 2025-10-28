b847e6e08de9cc0bd3a09de869c89e5d07c78150
Nathan Dunfield <nathan@dunfield.info>
2025-10-27 20:21:55 -0500

    Fix of last commit

--- setup.py
+++ setup.py
@@ -329,7 +329,7 @@ class package_fix_version_regina_config(Command):
 class package_download_tokyocabinet(SystemCommand):
     # Because of network issues, I've just added this to the repo.
     # system_commands = ['cd /tmp; curl -O %s' % tokyocabinet_uri]
-    pass
+    system_commands = []
 
 class package_download_libxml(SystemCommand):
     system_commands = ['cd /tmp; curl -O %s' % libxml_uri]
